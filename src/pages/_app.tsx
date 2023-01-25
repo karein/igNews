92c13d1326fe4f23427c8d9d90f71d899bd60156
karein <layla-poo@hotmail.com>
2023-01-25 15:50:16 -0300

    initializing project

---
+++ src/pages/_app.tsx
@@ -0,0 +1,16 @@
+import { AppProps } from "next/app"
+
+import { Header } from "../components/Header"
+
+import '../styles/global.scss';
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Header />
+      <Component {...pageProps} />
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/index.tsx
@@ -0,0 +1,30 @@
+import Head from 'next/head';
+
+import { SubscribeButton } from '../components/SubscribeButton';
+
+
+import styles from './home.module.scss'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Home | ig.news</title>
+      </Head>
+      <main className={styles.contentConteiner}>
+        <section className={styles.hero}>
+          <span>Hey, welcome 👏</span>
+          <h1>News about the <span>React</span> world</h1>
+          <p>
+            Get access to all the peblications <br />
+            <span>for $9.90 month</span>
+          </p>
+
+          <SubscribeButton />
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
+    </>
+  )
+}
